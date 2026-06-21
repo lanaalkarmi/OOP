34a0db1d8864154c3d913e9e472db4aae457a7d6
lana alkarmi <lanakarmi05@gmail.com>
2026-06-21 20:22:04 +0300

    Add files via upload

---
+++ oop1.cpp
@@ -0,0 +1,57 @@
+﻿
+#include <iostream>
+using namespace std;
+   
+//encapsulation 
+
+
+class account {
+private:
+    int id;
+    string name;
+    double balance;
+public:
+    void setname(string n) {
+        name = n;
+    }
+    string getname() {
+        return name;
+    }
+    void setid(int n) {
+        id = n;
+    }
+    int getid() {
+        return id;
+    }
+    void setbalance(double b) {
+        if (b < 0) {
+            balance = b * (-1);
+        }
+        else
+        balance = b;
+    }
+    double getbalance() {
+        return balance;
+    }
+    void print() {
+        cout << name << id << balance;
+    }
+};
+
+int main()
+{
+    account a;
+    a.setname("lolo");
+    a.setid(1);
+    a.setbalance(1200);
+    a.print();
+    cout << a.getname();
+    cout << a.getid();
+    cout << a.getbalance();
+
+
+
+
+
+}
+
