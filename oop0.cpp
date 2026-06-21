3857c1109c82b07dd85f30298ff7fb25c7396eb1
lana alkarmi <lanakarmi05@gmail.com>
2026-06-21 20:21:44 +0300

    Add files via upload

---
+++ oop0.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class rec {
+private:
+    int height;
+    int width;
+public:
+    void setwidth(int  w) {
+        if (w < 0)
+            width = -1 * w;
+        else
+            width = w;
+    }
+
+    int getw() {
+        return width;
+    }
+
+    void setheight(int h) {
+        if (h < 0)
+            height = -1 * h;
+        else
+            height = h;
+    }
+    x
+    int getheight() {
+        return height;
+    }
+
+
+    int calcArea() {
+        return height * width;
+    }
+};
+
+int main()
+{
+    rec s;
+    
+    s.setwidth(2);
+    s.setheight(-10);
+    cout << s.calcArea();
+}
+
