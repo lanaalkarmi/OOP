a739a9f1dfbe42c00670d26496f2db7f4fa8086f
lana alkarmi <lanakarmi05@gmail.com>
2026-06-23 18:11:39 +0300

    Add files via upload

---
+++ oop13.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+using namespace std;
+class calculater {
+public:
+    static double sum(int a, int b)
+    {
+        return a + b;
+    }
+    static double subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    static double multi(int a, int b)
+    {
+        return a * b;
+    }
+
+    static double divide(int a, int b)
+    {
+        return a / b;
+    }
+
+};
+
+int main()
+{
+    cout << calculater::sum(9, 10);
+}
+
