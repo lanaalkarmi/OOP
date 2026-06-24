0699d1b4a41273b5d06c2fb0aba379828415fb9a
lana alkarmi <lanakarmi05@gmail.com>
2026-06-24 16:32:22 +0300

    Add files via upload

---
+++ oop15.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+using namespace std;
+
+//pointer --> متغير محتواه عنوان (address)
+
+
+
+int main()
+{
+	int x = 7; 
+
+	//cout << x <<" "<< &x << endl;;// اي متغير بعمله بحجز مكان بالذاكرة مخزن بادريس معين
+	// لحتى اعرف وين مخزن بالذاكرة بستخدم اشارة الريفيرنس &\
+	//الريفيرنس هي عبارة عن ال address و لازم يكون بنفس نوع الاشي اللي باشر عليه
+	//بينعمل عشان ياشر عندي ع اشي معين
+	int* ptr = &x
+		
+		;// عملنا بوينتر عن طريق int *ptr و بعدها 
+	// حكيناله تعال اشر عندي عالادريس الحاص ب المتغير اكس &x
+	
+	cout << x << " " << &x << endl;
+	cout << ptr << " " << &ptr << endl;
+	// ptr --> it will print the address of x == (&x)
+	// & ptr --> it will print the address of the variable ptr 
+
+
+}
+
