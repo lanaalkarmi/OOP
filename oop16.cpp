c95ac8b7588d748f4f7c311d3a4664631ef6ead2
lana alkarmi <lanakarmi05@gmail.com>
2026-06-24 16:47:09 +0300

    Add files via upload

---
+++ oop16.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+//pointer --> متغير محتواه عنوان (address)
+//ليش لازم 
+
+
+int main()
+{
+	int x = 7;
+	int* ptr = &x;
+	cout << x << " " << &x << endl;
+	cout << ptr << endl;
+	cout << *ptr << endl;
+	// هاي يعني جيب القيمة اللي مخزنة بهاد الادريس 
+	*ptr = 6; // the value of x
+	// زي كانو غير المحتوى تاع اكس 
+	cout << x << " " << &x << endl;// الادريس بضل زيما هو لاني بس فيرت القيمة ما طبيت بالادريس
+	cout << ptr << endl;
+	cout << *ptr << endl;
+
+// لما اعمل اكتر من متغير من نوع int المسافة بينهم بتكون 4 بايت
+
+
+}
