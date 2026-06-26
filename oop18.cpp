d28fa6e7cbb03de4d1a420631658358fad50a77f
lana alkarmi <lanakarmi05@gmail.com>
2026-06-26 15:33:58 +0300

    Add files via upload

---
+++ oop18.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+//pointers with object
+
+class test {
+    int val;
+public:
+    void print() {
+        cout << val << endl;
+    }
+
+    void setval(int val) {
+       this-> val = val;// first val is the attribute & the sec int is the val
+    }
+};
+
+int main()
+{
+    test t;
+    t.setval(8);
+    t.print();
+
+}
+
