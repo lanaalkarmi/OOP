67e318987419ab12d2be9a0623d7aa1e2e868a28
lana alkarmi <lanakarmi05@gmail.com>
2026-06-27 17:14:33 +0300

    Add files via upload

---
+++ oop26.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+using namespace std;
+
+//template class
+
+template<class T>
+class employee {
+public:
+    T id;
+};
+
+int main()
+{
+    employee <string>i;
+    i.id = "957";
+    cout << i.id;
+   
+}
