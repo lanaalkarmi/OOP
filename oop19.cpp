01107e71296c8c7b736898582f34b2bf5af18104
lana alkarmi <lanakarmi05@gmail.com>
2026-06-27 10:56:45 +0300

    Add files via upload

---
+++ oop19.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+// الاري هي عبارة عن بوينتر ثابت باشر ع اول مكان بالارري 
+int main()
+{
+	int arr[] = { 1,2,3,4 };
+	cout << arr << endl;
+	cout << &arr[0] << endl;
+	cout << *arr << endl;
+	cout << arr + 1 << endl;
+	cout << arr + 2 << endl;
+	cout << *arr + 1 << endl;//1+1
+	cout << *arr + 2 << endl;//1+2
+	cout << *(arr + 1) << endl;// هون بدي يمشي ع كل الانديكس ف عملت الاقواس ك اولوية
+	cout << *(arr + 2) << endl;//3 index 2
+	cout << endl;
+	for (int i = 0; i < 4; i++) {
+		cout << *(arr + i) << endl;
+
+	}
+	for (int i = 0; i < 4; i++) {
+		cout << arr++ << endl; // هون بالبداية بخطرلي انو رح يطبع الادريس و يزيد عليه ع حسب قيمة الdata type 
+		// و لكن رح يعطي ايرور لانو الاري هو بوينتر ثابت ما بيتحرك
+		// ف بعمل بوينتر جديد و بحركه ع هوا هالاشي 
+
+	}
+	int* p = arr;
+	for (int i = 0; i < 4; i++) {
+		cout << p++ << endl;
+
+	}
+}
